7689ef21da3d69be4f846f52f33fcd1bcb7537c5
VOLKAN BAKIR <volkan_bakir@hotmail.com>
2021-08-15 23:36:56 +0300

    Project has been initialized with the course material

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const String _spartanMB = 'Spartan MB';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: _spartanMB,
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: _spartanMB,
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontFamily: _spartanMB,
+  fontSize: 30.0,
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
